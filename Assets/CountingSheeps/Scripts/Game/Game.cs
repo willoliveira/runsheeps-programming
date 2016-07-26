6a09058555fde483726dd2c7e58e8acd40cbb7d4
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-07-25 22:44:19 -0300

    Estrutura inicial do projeto

---
+++ Assets/CountingSheeps/Scripts/Game/Game.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	#region PUBLIC VARS
+	public GameObject GameOverScreen;
+	public Text ScoreText;
+	public SpawnSheeps mSpawnSheeps;
+	public GameObject SheepsContainer;
+	#endregion
+
+	#region PRIVATE VARS
+	private int Score = 0;
+	#endregion
+
+	#region PUBLIC METHODS
+	public void InitGame()
+	{
+
+	}
+
+	public void RestartGame()
+	{
+		foreach (Transform child in SheepsContainer.transform) {
+			Destroy(child.gameObject);
+		}
+		//limpa o array das ovelhas
+		mSpawnSheeps.Clear();
+		//fecha a tela do game over
+		GameOverScreen.SetActive(false);
+		//zera o score
+		Score = 0;
+		ScoreText.text = string.Format("{0:000}", Score);
+
+		Time.timeScale = 1;
+	}
+
+	public void OnGameOver()
+	{
+		Time.timeScale = 0;
+
+		GameOverScreen.SetActive(true);
+		
+		ScoreText.text = string.Format("{0:000}", Score);
+	}
+
+	public void OnScorePoint()
+	{
+		Score++;
+		ScoreText.text = string.Format("{0:000}", Score);
+		//
+		mSpawnSheeps.SheepScore();
+	}
+	#endregion
+}
---
+++ Assets/CountingSheeps/Scripts/Game/Player.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public float MaxSpeed = 5f;
+	public float JumpForce = 1000f;
+	public Transform GroundCheck;
+
+	private GameScreen mGameManager;
+
+	private bool Jump = false;
+	private bool Grounded = false;
+	private Animator anim;
+	private Rigidbody2D mRigidbody2D;
+
+	private int Score = 0;
+
+
+	// Use this for initialization
+	void Awake()
+	{
+		mGameManager = GameObject.Find("GameManager").GetComponent<GameScreen>();
+		mRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	public void JumpAction()
+	{
+		Grounded = Physics2D.Linecast(transform.position, GroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if (Grounded)
+		{
+			Jump = true;
+		}
+	}
+
+	// Update is called once per frame
+	//void Update()
+	//{
+	//	Grounded = Physics2D.Linecast(transform.position, GroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+	//	if (Input.GetButtonDown("Jump") && Grounded)
+	//	{
+	//		Jump = true;
+	//	}
+	//}
+
+	void FixedUpdate()
+	{
+		mRigidbody2D.velocity = new Vector2(MaxSpeed, mRigidbody2D.velocity.y);
+		if (Jump)
+		{
+			mRigidbody2D.AddForce(new Vector2(0f, JumpForce));
+			Jump = false;
+		}
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Wall")
+		{
+			mGameManager.OnGameOver();
+		}
+
+		if (coll.gameObject.tag == "DiscardSheep")
+		{
+			Destroy(transform.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "ScorePoint")
+		{
+			mGameManager.OnScorePoint();
+		}
+	}
+}
