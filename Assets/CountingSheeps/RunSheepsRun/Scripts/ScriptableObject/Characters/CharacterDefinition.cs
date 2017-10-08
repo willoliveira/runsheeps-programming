f7db9263a16d88c95bdf6ec38a9ca5390701898e
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-10-07 21:20:24 -0300

    [SpawnObstacles] InProgress - Mudando Character Definition de pasta

---
+++ Assets/CountingSheeps/RunSheepsRun/Scripts/ScriptableObject/Characters/CharacterDefinition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace CountingSheeps.RunSheepsRun
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "Character Definition")]
+    public class CharacterDefinition : ScriptableObject
+    {
+        public string nameCharacter;
+        public int order;
+        public Texture2D image;
+        public int price;
+        public bool isAvaliable;
+
+
+    }
+}
